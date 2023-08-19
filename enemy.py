b354c9b3db69747988905ef7c3ef61dfd6f176e2
David Ward <davidmward@mail.com>
2023-08-19 00:32:56 -0700

    start of class

--- enemy.py
+++ enemy.py
@@ -1,1 +1,10 @@
 import pygame
+
+# class inherets from pygame sprite class
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, enemyType):
+
+        # Initialize parent sprite class
+        super().__init__()
+        self.image = None
+        self.rect = None
