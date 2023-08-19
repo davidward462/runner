a799cd90e06e81dadc3e76a24e4822abed3c3bc7
David Ward <davidmward@mail.com>
2023-08-19 00:36:50 -0700

    determine enemy type

--- enemy.py
+++ enemy.py
@@ -6,5 +6,18 @@ class Enemy(pygame.sprite.Sprite):
 
         # Initialize parent sprite class
         super().__init__()
+
+        if enemyType == 'fly':
+            fly1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
+            fly2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+            self.frames = [fly1, fly2]
+            yPos = 210
+        else:
+            # If enemy is a snail
+            snail1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+            snail2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+            self.frames = [snail1, snail2]
+            yPos = 300
+
         self.image = None
         self.rect = None
